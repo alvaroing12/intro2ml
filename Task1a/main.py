0914a82ccca39cb14bc1073fbb9f1cebcdeeb915
gasimomitis <georgios.asimomitis@gmail.com>
2018-03-07 19:51:42 +0100

    Second commit

--- Task1a/main.py
+++ Task1a/main.py
@@ -1,8 +1,11 @@
 from utils import parsedata
 from utils import buildmodel
 from utils import preprocess
+from utils import formoutput
 import numpy as np
 from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import scatter_matrix
 
 X,npX = parsedata("train.csv")
 
@@ -15,11 +18,17 @@ scores = np.zeros((10,len(lambda_ridge)))
 count = 0
 
 kf = KFold(n_splits=10)
-for train, test in kf.split(npX):
+for train, test in kf.split(train_npX_pro):
     for l in range(len(lambda_ridge)):
         scores[count,l] = buildmodel(train_npX_pro[train,1:],train_npX_pro[test,1:],train_npX_pro[train,0],train_npX_pro[test,0],lambda_ridge[l])
     count+= 1
 
 
-#Correl = train_npX_pro.corr()
-#print np.unique(npTrain[:,11])
+formoutput(np.mean(scores, axis=0))
+
+
+##################################
+scatter_matrix(X.ix[:,2:11])
+plt.show()
+Correl = X.ix[:,2:11].corr()
+##################################
--- Task1a/utils.py
+++ Task1a/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import Ridge
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
 from math import sqrt
 from numpy.core.multiarray import dtype
 
@@ -17,7 +18,7 @@ def buildmodel(data,test,target_train,target_test,lambda_coeff):
 
     # Fit the model
 
-    ridgereg = Ridge(alpha=lambda_coeff, normalize=True)
+    ridgereg = Ridge(alpha=lambda_coeff, normalize=False,solver='sag')
     ridgereg.fit(data,target_train)
     y_pred = ridgereg.predict(test)
     rmse = sqrt(mean_squared_error(target_test, y_pred))
@@ -27,7 +28,11 @@ def buildmodel(data,test,target_train,target_test,lambda_coeff):
 def preprocess(data):
 
     res =  np.delete(data,10,1)
-    return res
+    res_scaled = preprocessing.scale(res)
+    return res_scaled
 
 
+def formoutput(arr):
 
+    np.savetxt("output.csv", arr)
+    return
